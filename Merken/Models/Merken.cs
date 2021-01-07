f4b5e5a696c3d3ccf59e1dc0ff4bac616121c6d4
MAURO GAY <MAURO.GAY@student.intecbrussel.be>
2021-01-07 17:34:13 +0100

    .

---
+++ Merken/Models/Merken.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Merken.Models
+{
+    public class Merken1
+    {
+        List<string> merken = new List<string>() ;
+
+        ArrayList arrayList = new ArrayList();
+        private Data data;
+
+        public Merken1(Data data)
+        {
+            merken = new List<string>();
+            ArrayList tempArrayList = new ArrayList(Data.Merken);
+            foreach (var item in tempArrayList)
+            {
+                merken.Add((string)item);
+
+            }
+        }
+
+    
+
+        public void sorteer()
+        {
+            merken.Sort() ;
+        }
+
+        public void keerOm()
+        {
+            merken.Reverse();
+        }
+
+        public override string ToString()
+        {
+            string listString = "";
+            foreach (var item in merken)
+            {
+                listString += item + " ";
+            }
+            return listString;
+        }
+    }
+}
---
+++ Merken/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Merken.Models;
+
+namespace Merken
+{
+    public class TestMerken
+    {
+        public static void Main(string[] args)
+        {
+            Data data = new Data();
+            Merken1 merken = new Merken1(data);
+            Console.WriteLine(merken);
+            merken.sorteer();
+            Console.WriteLine(merken);
+            merken.keerOm();
+            Console.WriteLine(merken);
+        }
+    }
+}
